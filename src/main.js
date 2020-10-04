17cf6827bc152706d0ea7f5d902bb003019f79bb
xiaguikun <2973738150@qq.com>
2020-10-04 15:38:18 +0800

    start-x

--- src/main.js
+++ src/main.js
@@ -3,10 +3,17 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import "./assets/reset.less"
+import "amfe-flexible";
+
+import { Button } from 'vant';
+
+Vue.use(Button);
+
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    render: h => h(App)
 }).$mount('#app')
