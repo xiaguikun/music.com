1f63999e0ee5d1778fad6a9f68ce2d4dd3331b7d
fookc <942307805@qq.com>
2020-10-12 21:12:34 +0800

    fkcfkcfkc122

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ import "./assets/set-c/mine-c.less"
 import "amfe-flexible";
 import "./assets/style.less";
 import "./assets/one.less";
-import BaiduMap from 'vue-baidu-map';
+// import BaiduMap from 'vue-baidu-map';
 
 
 
@@ -38,9 +38,9 @@ Vue.use(TreeSelect);
 Vue.use(Form);
 Vue.use(Field);
 
-Vue.use(BaiduMap, {
-    ak: 'prnThHq3IOti4KwEOF4UClQbGEez1wZS'
-})
+// Vue.use(BaiduMap, {
+//     ak: 'prnThHq3IOti4KwEOF4UClQbGEez1wZS'
+// })
 
 
 
